a70d72f56804e5bcc926588934004b9537210974
r.williams <you@personal.com>
2026-07-01 14:44:19 +0200

    initialise

---
+++ src/main/java/com/task/task_api/TaskApiApplication.java
@@ -0,0 +1,13 @@
+package com.task.task_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskApiApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(TaskApiApplication.class, args);
+	}
+
+}
